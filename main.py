e16ab8175c84292d99ce5019215a3bce1a4360b2
Anton Stamme <anton@stamme.de>
2022-02-19 12:35:31 +0100

    demo induction loop readings

--- main.py
+++ main.py
@@ -22,8 +22,24 @@ step = 0
 
 while step < 3600:  # traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
-    # if traci.inductionloop.getLastStepVehicleNumber("instantInductionLoop_-11627689_0_0") > 0:
-    #     traci.trafficlight.setRedYellowGreenState("0", "GrGr")
+
+    # [(veh_id, veh_length, entry_time, exit_time, vType), ...]
+    vehicleData = traci.inductionloop.getVehicleData("inductionLoop_test2")
+
+    for (vehicleId, _, _, _, vehicleType) in vehicleData:
+        co2Emission = traci.vehicle.getCO2Emission(vehicleId)
+        timeLoss = traci.vehicle.getTimeLoss(vehicleId)
+
+        idString = f'vehicle: {vehicleId}'
+        typeString = f'type: {vehicleType}'
+        co2String = f'CO2 emissions: {"%.0f" % co2Emission} mg/s'
+        timeLossString = f'time loss: {"%.1f" % timeLoss} s'
+
+        print(f"{idString:<40}{typeString:<40}{co2String:<40}{timeLossString:<40}")
+
+    # if traci.inductionloop.getLastStepVehicleNumber("inductionLoop_test2") > 0:
+        # traci.trafficlight.setRedYellowGreenState("0", "GrGr")
+
     step += 1
 
 traci.close()
